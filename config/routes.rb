1d4fb0c889af60358f6627e38ccd646c1d069395
Haruka W <nekonoko1995@gmail.com>
2020-05-15 18:45:57 +0900

    [Add]Usersコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
   root 'homes#top'
+
+  # 社員コントローラ
   devise_for :users
+  resources :users, only: %i[index show edit update] do
+  	# is_activeカラムを「0:在籍」から「1:退職」へ変更するルーティング
+  	collection do
+      patch 'active'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
