73ca2c9d1fac7fc5d3e236205b6f91fb53662356
Haruka W <nekonoko1995@gmail.com>
2020-05-15 19:21:26 +0900

    [Add]ヘッダー作成（ログアウトのリンク設置）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後はログインユーザーのshowページへ遷移
+  def after_sign_in_path_for(resource)
+  	user_path(resource)
+  end
+  # ログアウト後はトップページへ遷移
+  def after_sign_out_path_for(resource)
+  	root_path
+  end
+
   protected
   # 社員登録時に氏名のデータ操作を許可
   def configure_permitted_parameters
