1359090dc8408f03a3a9663245c8cdef9389747e
Haruka W <nekonoko1995@gmail.com>
2020-05-16 23:25:18 +0900

    [Add]社員情報の変更機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 0が在籍中の社員、1が退職した社員
+  enum is_active: { 在籍中: 0, 退職済み: 1 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,7 @@ Rails.application.routes.draw do
 
   # 社員コントローラ
   devise_for :users
-  resources :users, only: %i[index show edit update] do
-  	# is_activeカラムを「0:在籍」から「1:退職」へ変更するルーティング
-  	collection do
-      patch 'active'
-    end
-  end
+  resources :users, only: %i[index show edit update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
