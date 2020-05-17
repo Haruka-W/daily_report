a524fa6339b2006be593f2e274ea2c5abaebb4ec
Haruka W <nekonoko1995@gmail.com>
2020-05-17 19:04:26 +0900

    [Add]Placeモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_083942) do
+ActiveRecord::Schema.define(version: 2020_05_17_095842) do
+
+  create_table "places", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
