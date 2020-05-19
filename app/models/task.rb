3e36cb7b83add62e5951eecccc42de40d71b2987
Haruka W <nekonoko1995@gmail.com>
2020-05-19 22:02:48 +0900

    [Add]progress_statusカラムにenum追記

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,19 @@
 class Task < ApplicationRecord
 	belongs_to :project
+
+	# 進捗ステータス
+	enum progress_status: {
+		０％: 0,
+		１０％: 1,
+		２０％: 2,
+		３０％: 3,
+		４０％: 4,
+		５０％: 5,
+		６０％: 6,
+		７０％: 7,
+		８０％: 8,
+		９０％: 9,
+		１００％: 10
+	}
+
 end
